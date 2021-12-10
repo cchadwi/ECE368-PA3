a44bc48b69afe6e37c8798a98e6584abf79b9e68
Connor Gregory Chadwick <cchadwi@ecegrid-thin1.ecn.purdue.edu>
2021-12-09 22:17:23 -0500

    minheap

--- pa3/minheap.h
+++ pa3/minheap.h
@@ -21,19 +21,20 @@ struct MinHeap
     struct Path **array;
 };
 
-struct MinHeap* createHeap(int size)
+struct MinHeap* makeHeap(int size)
 {
     struct MinHeap* minHeap = (struct MinHeap*)malloc(sizeof(struct MinHeap));
     minHeap -> array = (struct Path**)malloc(size * sizeof(struct Path*));
     minHeap -> size = 0;
-    int i;
 
+    int i;
     for(i = 0; i < size; i++)
     {
         minHeap -> array[i] = newPath(i, INT_MAX);
         minHeap -> size += 1;
     }
-    return(minHeap);
+
+    return minHeap;
 };
 
 struct Path* extract(struct MinHeap* minHeap, int **pos, int length)
@@ -42,14 +43,17 @@ struct Path* extract(struct MinHeap* minHeap, int **pos, int length)
     {
         retrn NULL;
     }
+
     struct Path* root = minHeap -> array[0];
     int prevPos = minHeap -> size - 1;
     struct Path* prevNode = minHeap -> array[prevPos];
     minHeap -> array[0] = prevNode;
+
     (*pos)[root -> vertex] = prevNode;
     (*pos)[prevNode -> vertex] = 0;
     minHeap -> size -= 1;
-    downHeapify(minHeap, 0, pos, length);
+
+    downHeapify(minHeap, pos, length, 0);
 };
 
 void swapPath(struct Path** x, struct Path** y)
@@ -59,3 +63,109 @@ void swapPath(struct Path** x, struct Path** y)
     *y = temp;
     return;
 }
+
+void downHeapify(struct MinHeap* minHeap, int **pos, int length, int index)
+{
+    int smallest;
+    int left;
+    int right;
+    struct Path *smallestNode;
+    struct Path *indexNode;
+    smallest = index;
+
+    left = 2 * index + 1;
+    right = 2 * index + 2;
+    int size = minHeap -> size;
+    int shortPath = minHeap -> array[smallest] -> path;
+
+    if(minHeap -> array[left] -> path < shortPath && left < size)
+    {
+        if(minHeap -> array[left] -> path < shortPath)
+        {
+            smallest = left;
+        }
+        else
+        {
+            smallest = index;
+        }
+    }
+
+    int newIndex = smallest;
+    shortPath = minHeap -> array[smallest] -> path;
+
+    if(minHeap -> size > right)
+    {
+        if(minHeap -> array[right] -> path < shortPath)
+        {
+            smallest = right;
+        }
+        else
+        {
+            smallest = newIndex;
+        }
+    }
+    
+    smallestNode = minHeap -> array[smallest];
+    indexNode = minHeap -> array[index];
+
+    if(smallest != index)
+    {
+        (*pos)[smallestNode -> vertex] = index;
+        (*pos)[indexNode -> vertex] = smallest;
+        swapPath(&minHeap -> array[smallest], &minHeap -> array[index]);
+        downHeapify(minHeap, pos, length, smallest);
+    }
+    return;
+}
+
+void update(struct MinHeap* minHeap, int dest, int path, int pos, int **posArray)
+{
+    int parent;
+    int child;
+    int pIndex;
+    int cIndex;
+    int index = pos;
+
+    while(index > 0)
+    {
+        parent = (index - 1) / 2;
+        child = index;
+
+        if(minHeap -> array[parent] -> path > minHeap -> array[child] -> path)
+        {
+            pIndex = minHeap -> array[parent] -> vertex;
+            cIndex = minHeap -> array[child] -> vertex;
+
+            (*posArray)[pIndex] = parent;
+            (*posArray)[cIndex] = child;
+
+            swapPath(&minHeap -> array[child], &minHeap -> array[parent]);
+        }
+    }
+    return;
+}
+
+bool isEmpty(struct MinHeap* minHeap)
+{
+    if(minHeap -> size != 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+bool inQueue(struct MinHeap* minHeap, int i)
+{
+    if(i < minHeap -> size)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
