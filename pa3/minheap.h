308790991656d2a1e380d6d2de416f1b0d9b42f8
Connor Gregory Chadwick <cchadwi@ecegrid-thin1.ecn.purdue.edu>
2021-12-09 22:27:44 -0500

    minor edits

--- pa3/minheap.h
+++ pa3/minheap.h
@@ -26,14 +26,12 @@ struct MinHeap* makeHeap(int size)
     struct MinHeap* minHeap = (struct MinHeap*)malloc(sizeof(struct MinHeap));
     minHeap -> array = (struct Path**)malloc(size * sizeof(struct Path*));
     minHeap -> size = 0;
-
     int i;
     for(i = 0; i < size; i++)
     {
         minHeap -> array[i] = newPath(i, INT_MAX);
         minHeap -> size += 1;
     }
-
     return minHeap;
 };
 
@@ -54,6 +52,7 @@ struct Path* extract(struct MinHeap* minHeap, int **pos, int length)
     minHeap -> size -= 1;
 
     downHeapify(minHeap, pos, length, 0);
+    return root;
 };
 
 void swapPath(struct Path** x, struct Path** y)
@@ -71,10 +70,11 @@ void downHeapify(struct MinHeap* minHeap, int **pos, int length, int index)
     int right;
     struct Path *smallestNode;
     struct Path *indexNode;
+    
     smallest = index;
-
     left = 2 * index + 1;
     right = 2 * index + 2;
+    
     int size = minHeap -> size;
     int shortPath = minHeap -> array[smallest] -> path;
 
@@ -112,6 +112,7 @@ void downHeapify(struct MinHeap* minHeap, int **pos, int length, int index)
     {
         (*pos)[smallestNode -> vertex] = index;
         (*pos)[indexNode -> vertex] = smallest;
+
         swapPath(&minHeap -> array[smallest], &minHeap -> array[index]);
         downHeapify(minHeap, pos, length, smallest);
     }
@@ -125,6 +126,7 @@ void update(struct MinHeap* minHeap, int dest, int path, int pos, int **posArray
     int pIndex;
     int cIndex;
     int index = pos;
+    minHeap -> array[index] -> path = path;
 
     while(index > 0)
     {
@@ -141,6 +143,7 @@ void update(struct MinHeap* minHeap, int dest, int path, int pos, int **posArray
 
             swapPath(&minHeap -> array[child], &minHeap -> array[parent]);
         }
+        index = (index - 1) / 2;
     }
     return;
 }
